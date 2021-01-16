206838add1466c1d8dd50b99a785dcd9b8296f74
匡史 松本 <tad@viva-life.biz>
2021-01-16 11:38:46 +0900

    DBContextクラスの実装

---
+++ XamarinTodo.SQLite/Contexts/MyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using XamarinTodo.SQLite.Models;
+
+namespace XamarinTodo.SQLite.Contexts
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoDataModel> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TodoDataModel>();
+        }
+    }
+}
---
+++ XamarinTodo.SQLite/Models/TodoDataModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace XamarinTodo.SQLite.Models
+{
+    [Table("Todo")]
+    public class TodoDataModel
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(20)]
+        public string Title { get; set; }
+
+        public DateTime? Deadline { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
