ae2b7f0b94d49b87c3cfdd3501e43bbcc0f35c0c
匡史 松本 <tad@viva-life.biz>
2021-01-21 18:18:28 +0900

    Add: TodoPageModelを追加。

---
+++ XamarinTodo/XamarinTodo/Models/Todos/TodoPageModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinTodo.Models.Todos
+{
+    public class TodoPageModel
+    {
+        public TodoPageModel()
+        {
+
+        }
+
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime? Deadline { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
