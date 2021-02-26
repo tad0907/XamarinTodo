94969d4803374f12faf34ec41280fa1462eda419
匡史 松本 <tad@viva-life.biz>
2021-02-26 09:00:21 +0900

    微修正

--- XamarinTodo.Domain/Exceptions/ArgumentNullOrEmptyException.cs
+++ XamarinTodo.Domain/Exceptions/ArgumentNullOrEmptyException.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace XamarinTodo.Domain.Exceptions
 {
-    class ArgumentNullOrEmptyException : Exception
+    public class ArgumentNullOrEmptyException : Exception
     {
         public ArgumentNullOrEmptyException()
         {
